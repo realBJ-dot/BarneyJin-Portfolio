ef80e05fc2a8385f7072631af49f2eeab0ffffc5
realBJ-dot <peiyuan3@illinois.edu>
2026-09-06 17:30:12 -0500

    updated resume

--- components/ModernPortfolio.tsx
+++ components/ModernPortfolio.tsx
@@ -1,7 +1,7 @@
 import { ArrowUpRight, ArrowDown } from 'lucide-react'
 
 const links = {
-    resume: 'https://drive.google.com/drive/my-drive?dmr=1&ec=wgc-drive-%5Bmodule%5D-goto',
+    resume: 'https://drive.google.com/file/d/1HqiHi9RVQOLlWNUWLemUslWAicyzVOd6/view?usp=sharing',
     github: 'https://github.com/realBJ-dot',
     linkedin: 'https://www.linkedin.com/in/barneyjin/',
 }
